a4a3066c1ba6417790def5948b6622eab41662e2
Adam <aearanha@hotmail.com>
2021-09-27 14:29:25 -0400

    Working on Date and Weather component

--- src/components/DateWeather/DateWeather.js
+++ src/components/DateWeather/DateWeather.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function DateWeather() {
     return (
         <div className='dateWeather'>
-
+            <div></div>
+            <div></div>
         </div>
     )
 }
