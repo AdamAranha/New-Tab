461b1cce6d7bc5f3d992786091582958d1884e6d
Adam <aearanha@hotmail.com>
2021-09-28 20:46:59 -0400

    Updated to cache weather data in local storage. Weather updates every 30 minutes while page is live. Refreshing or opening a new page will NOT send another API request unless a valid value does not exist.

--- src/components/TimeDateWeather/TimeDateWeather.js
+++ src/components/TimeDateWeather/TimeDateWeather.js
@@ -35,34 +35,50 @@ export default function TimeDateWeather() {
     useEffect(() => {
         getTime();
         getDay();
-        getWeather();
+        checkWeatherValue();
 
-        function getTime() {
-            setTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-        }
-        function getDay() {
-            const dateObj = new Date();
-            setDate(`${dayArray[dateObj.getDay()]}, ${monthArray[dateObj.getMonth()]} ${dateObj.getDate()}.`);
-        }
-        function getWeather() {
-            fetch('https://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&appid=c017c4d551f1c62d18088f7e1024cd1f')
-                .then(res => res.json())
-                .then(result => {
-                    console.log(result);
-                    setWeather(Math.round(result.main.temp) + '°C');
-                });
-        }
-
-        let clock = setInterval(() => getTime(), 1000);
-        let calendar = setInterval(() => getDay(), 60000);
-        let weather = setInterval(() => getWeather(), 300000);
+        let clock = setInterval(() => getTime(), 1000); //Occurs every second
+        let calendar = setInterval(() => getDay(), 60000); //Occurs every minute
+        let checkWeather = setInterval(() => checkWeatherValue(), 1800000); //Occurs every 30 minutes
 
         return function cleanup() {
             clearInterval(clock);
             clearInterval(calendar);
-            // clearInterval(weather);
+            clearInterval(checkWeather);
         };
-    })
+    }, [])
+
+    function getTime() {
+        setTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+    }
+
+    function getDay() {
+        const dateObj = new Date();
+        setDate(`${dayArray[dateObj.getDay()]}, ${monthArray[dateObj.getMonth()]} ${dateObj.getDate()}.`);
+    }
+
+    function checkWeatherValue() {
+        let date = new Date();
+        const storedValue = JSON.parse(window.localStorage.getItem('weatherValue'));
+        const weatherValue = {
+            timestamp: date.getTime(),
+            temp: null
+        }
+
+        if (storedValue === null || ((date.getTime() - storedValue.timestamp) / 60000) >= 30) {
+            someWeather()
+                .then(result => {
+                    weatherValue.temp = result;
+                    setWeather(Math.floor(result));
+                    window.localStorage.setItem('weatherValue', JSON.stringify(weatherValue));
+                });
+        }
+        async function someWeather() {
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&appid=c017c4d551f1c62d18088f7e1024cd1f');
+            const weather = await response.json();
+            return weather.main.temp;
+        }
+    }
 
     return (
         <section className='timedateweather'>
@@ -70,15 +86,12 @@ export default function TimeDateWeather() {
                 {time}
             </p>
             <div className='dateWeather'>
-                <p className='date'>
-                    {date}
-                </p>
+                <p className='date'>{date}</p>
                 <div className='weather-container'>
                     <img className='weather-img' src={Cloud} alt='cloud' />
-                    <p className='weather'>24°C</p>
+                    <p className='weather'>{weather}°C</p>
                 </div>
             </div>
-
         </section>
     )
 }
